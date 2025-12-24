1b656425e47a587880f7db21b1bc476368db74c0
Hojin30 <ozancnn30@gmail.com>
2025-12-24 22:22:52 +0300

    Update cache name to seansnotu-cache-v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "seansnotu-cache-v2";
+const CACHE_NAME = "seansnotu-cache-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
 
 self.addEventListener("install", (event) => {
